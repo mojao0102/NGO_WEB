1a94f286a20434a73391ab4dcc58e6c5f5b26f03
jnxlumi <janixgame@gmail.com>
2026-06-09 11:31:38 +0800

    Student app: added student_edit, _list, _view .html

--- students/views.py
+++ students/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+# reference from 
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+
+def course_list(request):
+    return render(request, "course_list.html")
+
+def course_create(request):
+    return render(request, "course_edit.html")
+
+def course_edit(request, course_id):
+    return render(request, "course_edit.html")
+
+def course_view(request, course_id):
+    return render(request, "course_view.html")
